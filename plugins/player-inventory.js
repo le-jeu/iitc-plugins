20eeb2eb3da0e4259c933a599fcd18b44d373114
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-02-11 11:25:19 +0100

    add some doc

--- plugins/player-inventory.js
+++ plugins/player-inventory.js
@@ -46,10 +46,31 @@ const isKey = function (obj) {
 	return obj.resource && obj.resource.resourceType == "PORTAL_LINK_KEY";
 }
 
+const is
+
 const parsePortalLocation = function (location) {
 	return [lat, lng] = location.split(',').map(a => (Number.parseInt(a,16)&(-1))*1e-6);
 }
 
+/*
+{
+	"resource": {
+		"resourceType": "PORTAL_LINK_KEY",
+		"resourceRarity": "VERY_COMMON"
+	},
+	"portalCoupler": {
+		"portalGuid": "...",
+		"portalLocation": "int32 hex,int32 hex",
+		"portalImageUrl": "...",
+		"portalTitle": "...",
+		"portalAddress": "..."
+	},
+	"inInventory": {
+		"playerId": "...",
+		"acquisitionTimestampMs": "..."
+	}
+}
+*/
 const parsePortalKey = function (key) {
 	const data = {
 		type: "PORTAL_LINK_KEY",
@@ -61,6 +82,129 @@ const parsePortalKey = function (key) {
 	return data;
 }
 
+// {
+// 	"resourceWithLevels": {
+// 		"resourceType": "EMITTER_A",
+// 		"level": 7
+// 	}
+// }
+const parseLevelItem = function (item) {
+	return {
+		type: item.resourceWithLevels.resourceType,
+		level: item.resourceWithLevels.level,
+		count: 1,
+	}
+}
+
+/*
+{
+	"modResource": {
+		"displayName": "SoftBank Ultra Link",
+		"stats": {
+			"LINK_RANGE_MULTIPLIER": "5000",
+			"LINK_DEFENSE_BOOST": "1500",
+			"OUTGOING_LINKS_BONUS": "8",
+			"REMOVAL_STICKINESS": "150000",
+			...
+
+			"BURNOUT_INSULATION": "4",
+			"HACK_SPEED": "200000",
+			"ATTACK_FREQUENCY": "1500",
+			"HIT_BONUS": "200000",
+			"REMOVAL_STICKINESS": "200000",
+			"XM_SPIN": "-1"
+		},
+		"rarity": "VERY_RARE",
+		"resourceType": "ULTRA_LINK_AMP"
+	}
+}
+*/
+const parseMod = function (mod) {
+	return {
+		type: mod.modResource.resourceType,
+		name: mod.modResource.displayName,
+		count: 1,
+	}
+}
+
+/*
+{
+	"resource": {
+		"resourceType": "FLIP_CARD",
+		"resourceRarity": "VERY_RARE"
+	},
+	"flipCard": {
+		"flipCardType": "JARVIS"
+	}
+}
+*/
+const parseFlipCard = function (flipcard) {
+	return {
+		type: flipcard.resource.resourceType,
+		name: flipcard.flipCard.flipCardType,
+		count: 1,
+	}
+}
+
+
+/*
+{
+	"resourceWithLevels": {
+		"resourceType": "MEDIA",
+		"level": 1
+	},
+	"imageByUrl": {
+		"imageUrl": "http://lh3.googleusercontent.com/l62x6RqXSc0JZESahVtmbUOdLFDPAwVUaxx9kfOkAu98HA7bnU0mOftOV10qzgd_tO7dA_chiZHmG8YxfN0F"
+	},
+	"inInventory": {
+		"playerId": "redacted",
+		"acquisitionTimestampMs": "redacted"
+	},
+	"displayName": {
+		"displayName": "Media"
+	},
+	"storyItem": {
+		"primaryUrl": "https://youtu.be/4MyMpzkcYmk",
+		"shortDescription": "UmbraDefeat",
+		"mediaId": "4176",
+		"hasBeenViewed": false,
+		"releaseDate": "1571122800000"
+	}
+*/
+const parseMedia = function (media) {
+	return {
+		type: media.resourceWithLevels.resourceType,
+		mediaId: media.storyItem.mediaId,
+		name: media.storyItem.shortDescription,
+	}
+}
+/*
+{
+	"resource": {
+		"resourceType": "INTEREST_CAPSULE",
+		"resourceRarity": "VERY_RARE"
+	},
+	"moniker": {
+		"differentiator": "12345678"
+	},
+	"container": {
+		"currentCapacity": 100,
+		"currentCount": 0,
+		"stackableItems": [
+			{
+				"itemGuids": [...],
+				"exampleGameEntity": ["...", 0, {
+					<ITEMDATA>,
+					"displayName": {
+						"displayName": "Portal Shield",
+						"displayDescription": "Mod which shields Portal from attacks."
+					}
+				}]
+			},
+		]
+	}
+}
+*/
 const parseContainer = function (container) {
 	const containerName = container.moniker.differentiator;
 	const data = {
@@ -77,13 +221,30 @@ const parseContainer = function (container) {
 	}
 	return data;
 };
-
+/*
+[
+	guid, timestamp?, item object
+]
+*/
 const parseItem = function (item) {
 	const [id, ts, obj] = item;
+	if (obj.resourceWithLevels)
+		return parseLevelItem(obj);
+	if (obj.modResource)
+		return parseMod(obj);
+	if (obj.flipCard)
+		return parseFlipCard(obj);
 	if (obj.container)
 		return parseContainer(obj);
 	if (isKey(obj))
 		return parsePortalKey(obj);
+	if (obj.storyItem)
+		return parseMedia(obj);
+	if (obj.resource)
+		return {
+			type: obj.resource.resourceType,
+			count: 1
+		}
 	// xxx: other types
 };
 
