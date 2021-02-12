55a199a1ffa0dbd2582de4687a5586e904237892
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-02-12 19:24:50 +0100

    0.2.6

--- plugins/player-inventory.js
+++ plugins/player-inventory.js
@@ -1,7 +1,7 @@
 // @author         jaiperdu
 // @name           Player Inventory
 // @category       Info
-// @version        0.2.5
+// @version        0.2.6
 // @description    View inventory
 
 // stock intel
