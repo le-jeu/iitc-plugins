27bb6b7ba6635b09d559078b80edf7009f447fa7
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-06-29 19:18:05 +0200

    add gl layer for cache

---
+++ plugins/glify-layer.js
@@ -0,0 +1,78 @@
+// @author         jaiperdu
+// @name           Glify layer
+// @category       Map Tiles
+// @version        0.1.0
+// @description    GL layer
+
+var glLayers = {};
+
+var toClicks = {};
+function onPortalSelected(data) {
+  var guid = data.selectedPortalGuid;
+  if (guid && toClicks[guid]) {
+    delete toClicks[guid];
+    window.portals[guid].fire('click');
+  }
+}
+
+var colors = {
+  E: {r: 0, g: 1, b: 0, a: .5},
+  R: {r: 0, g: 0, b: 1, a: .5},
+  N: {r: 1, g: .4, b: 0, a: .5},
+};
+
+glLayers.layer = null;
+function showCache() {
+  if (window.plugin.cachePortals && window.plugin.cachePortals.db) {
+    var tx = window.plugin.cachePortals.db.transaction("portals", "readonly");
+    tx.objectStore("portals").getAll().onsuccess = function (event) {
+      var portals = event.target.result;
+      if (portals.length > 0) {
+        glLayers.layer = L.glify.points({
+          map: map,
+          pane: "cache",
+          data: portals.map((p) => [p.latE6/1e6, p.lngE6/1e6, p.guid]),
+          size: 10,
+          color: (i) => colors[portals[i].team || "N"],
+          click: (e, feature) => {
+            var guid = feature[2];
+            if (guid in window.portals) {
+              window.portals[guid].fire('click');
+              return;
+            }
+            toClicks[guid] = true;
+            window.renderPortalDetails(guid);
+          },
+        });
+      }
+    };
+  } else {
+    setTimeout(showCache, 2000);
+  }
+}
+
+function setup() {
+  try {
+    '@include_raw:external/glify-browser.js@';
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+
+  $('<style>').prop('type', 'text/css').html('.leaflet-pane.leaflet-cache-pane { z-index: 300; }').appendTo('head');
+
+  window.map.createPane('cache');
+  window.addLayerGroup('GL Cache', new (L.Layer.extend({
+    onAdd: function (map) {
+      if (!glLayers.layer) showCache();
+    },
+    onRemove: function (map) {
+      if (glLayers.layer) {
+        glLayers.layer.remove();
+        delete glLayers.layer;
+      }
+    },
+  })), true);
+
+  window.addHook("portalSelected", onPortalSelected);
+}
