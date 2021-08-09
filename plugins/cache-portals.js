0fb52ca73e66adc888523b2c8ed345deffb5c47b
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-08-09 15:00:07 +0200

    cache: drop location history thing

--- plugins/cache-portals.js
+++ plugins/cache-portals.js
@@ -1,7 +1,7 @@
 // @author         jaiperdu
 // @name           Cache visible portals
 // @category       Cache
-// @version        0.4.2
+// @version        0.4.3
 // @description    Cache the data of visible portals and use this to populate the map when possible
 
 // use own namespace for plugin
@@ -14,17 +14,16 @@ cachePortals.settings = {
   injectPortals: false,
   injectZoom: 15, // do whatever you want...
   storeTeam: false,
-  maxAge: 12, // hour, because team is game state
   maxLocationAge: 30, // days
 };
 
 function openDB() {
-  var rq = window.indexedDB.open("cache-portals", 6);
+  var rq = window.indexedDB.open("cache-portals", 7);
   rq.onupgradeneeded = function (event) {
     var db = event.target.result;
     if (event.oldVersion < 1) {
       var store = db.createObjectStore("portals", { keyPath: "guid" });
-      store.createIndex("latLngE6", ["latE6", "lngE6"], { unique: true });
+      store.createIndex("latLngE6", ["latE6", "lngE6"], { unique: false });
       store.createIndex("loadtime", "loadtime", { unique: false });
     }
     if (event.oldVersion < 2) {
@@ -44,30 +43,15 @@ function openDB() {
         store.createIndex(key, key, { unique: false });
       }
     }
-    if (event.oldVersion < 3) {
-      var store = db.createObjectStore("portals_history", { keyPath: "id", autoIncrement: true });
-      store.createIndex("guid", "guid", { unique: false });
-    }
-    if (event.oldVersion < 4) {
+    if (1 <= event.oldVersion && event.oldVersion < 4) {
       // allow co-located portals
       var store = rq.transaction.objectStore('portals');
       store.deleteIndex("latLngE6");
       store.createIndex("latLngE6", ["latE6", "lngE6"], { unique: false });
+      // this index is not used yet...
     }
-    if (event.oldVersion < 5) {
-      var store = rq.transaction.objectStore('portals_history');
-      store.openCursor().onsuccess = function (e) {
-        var cursor = e.target.result;
-        if (cursor) {
-          var portal = cursor.value;
-          if (!portal.date) portal.date = portal.lastSeen;
-          if (!portal.timestamp) portal.timestamp = portal.date;
-          store.put(portal);
-          cursor.continue();
-        }
-      };
-
-      store.createIndex("lastSeen", "lastSeen", { unique: false });
+    if (3 <= event.oldVersion && event.oldVersion < 6) {
+      db.deleteObjectStore('portals_history');
     }
   };
   rq.onsuccess = function (event) {
@@ -84,8 +68,8 @@ function openDB() {
 function cleanup() {
   if (!cachePortals.db) return;
   console.time("cache-portals: cleanup");
-  var maxAge = Date.now() - cachePortals.settings.maxAge * 60 * 60 * 1000;
-  var tx = cachePortals.db.transaction(["portals", "portals_history"], "readwrite");
+  var maxAge = Date.now() - cachePortals.settings.maxLocationAge * 24 * 60 * 60 * 1000;
+  var tx = cachePortals.db.transaction("portals", "readwrite");
   tx
     .objectStore("portals")
     .index("loadtime")
@@ -98,81 +82,10 @@ function cleanup() {
       cursor.continue();
     }
   };
-  maxAge = Date.now() - cachePortals.settings.maxLocationAge * 24 * 60 * 60* 1000;
-  tx
-    .objectStore("portals_history")
-    .index("lastSeen")
-    .openKeyCursor(window.IDBKeyRange.upperBound(maxAge)).onsuccess = function (
-    event
-  ) {
-    var cursor = event.target.result;
-    if (cursor) {
-      tx.objectStore("portals_history").delete(cursor.primaryKey);
-      cursor.continue();
-    }
-  };
 
   tx.oncomplete = function () {
     console.timeEnd("cache-portals: cleanup");
-  }
-}
-
-function updateLocationHistory(portals) {
-  var tx = cachePortals.db.transaction("portals_history", "readwrite");
-  var store = tx.objectStore("portals_history");
-  var index = store.index("guid");
-
-  var count = portals.length;
-
-  console.time("cache-portals: updateLocationHistory");
-  tx.oncomplete = () => {
-    console.timeEnd("cache-portals: updateLocationHistory");
-    console.log("cache-portals: updateLocationHistory:", count, "portals");
-  }
-
-  portals.forEach((portal) => {
-    // ignore some forged data
-    if (!portal.timestamp) return;
-    index.getAll(portal.guid).onsuccess = function (event) {
-      var portals = event.target.result;
-      if (portals.length > 0) {
-        var last = portals[portals.length-1];
-        // ignore old data (whatever the source)
-        if (last.timestamp > portal.timestamp) return
-        // update last seen
-        if (last.latE6 === portal.latE6 && last.lngE6 === portal.lngE6) {
-          last.lastSeen = Date.now();
-          last.timestamp = portal.timestamp;
-          store.put(last)
-          return;
-        }
-        window.map.fire('portalChange:location', { prev: last, cur: portal });
-        var latLng = [
-          (portal.latE6*1e-6).toFixed(6),
-          (portal.lngE6*1e-6).toFixed(6)
-        ];
-        var link = window.makePermalink(latLng);
-        var div = L.DomUtil.create('div');
-        div.append("Portal ")
-        var a = L.DomUtil.create('a', '', div);
-        a.textContent = portal.guid;
-        L.DomEvent.on(a, 'click', (ev) => {
-          L.DomEvent.preventDefault();
-          window.zoomToAndShowPortal(portal.guid, latLng);
-        });
-        div.append(` has moved from ${(last.latE6*1e-6).toFixed(6)},${(last.lngE6*1e-6).toFixed(6)} to ${latLng.join(',')}`);
-        alert(div, true);
-      }
-      store.add({
-        guid: portal.guid,
-        latE6: portal.latE6,
-        lngE6: portal.lngE6,
-        date: portal.timestamp,
-        timestamp: portal.timestamp,
-        lastSeen: Date.now(),
-      });
-    };
-  });
+  };
 }
 
 function putPortals(portals) {
@@ -187,7 +100,6 @@ function putPortals(portals) {
     console.log("cache-portals: putPortals:", count, "portals");
   }
   portals.forEach((portal) => store.put(portal));
-  updateLocationHistory(portals);
 }
 
 function putPortal(portal) {
@@ -213,9 +125,17 @@ function portalDetailLoaded(data) {
 
 function mapDataRefreshEnd() {
   var portals = [];
+  var linkPortals = {};
+  // real placeholder
+  for (var guid in window.links) {
+    var link = window.links[guid];
+    linkPortals[link.options.data.oGuid] = true;
+    linkPortals[link.options.data.dGuid] = true;
+  }
   for (var guid in window.portals) {
     var options = window.portals[guid].options;
-    if (options.data.level === undefined) continue;
+    // skip our injected portals
+    if (!linkPortals[guid] && options.data.level === undefined) continue;
     var portal = {
       guid: options.guid,
       team: options.data.team,
@@ -276,6 +196,9 @@ function entityInject(data) {
   var lowerTile = coordsToTile(lowerBound[0], lowerBound[1], mapZoom);
   var upperTile = coordsToTile(upperBound[0], upperBound[1], mapZoom);
 
+  var tiles = (upperTile[0] - lowerTile[0] + 1) * (upperTile[1] - lowerTile[1] + 1);
+  var count = 0;
+  console.time("cache-portals: inject portals");
   for (var x = lowerTile[0]; x <= upperTile[0]; x++) {
     for (var y = lowerTile[1]; y <= upperTile[1]; y++) {
       var ents = [];
@@ -290,6 +213,12 @@ function entityInject(data) {
             ]),
             "core"
           );
+        }
+        tiles -= 1;
+        count += portals.length;
+        if (tiles === 0) {
+          console.timeEnd("cache-portals: inject portals");
+          console.log("cache-portals:", count, "portals injected");
         }
       };
     }
@@ -325,11 +254,6 @@ function displayOpt() {
   addInput(div, 'checkbox', "Use cache to populate the map", 'injectPortals');
   addInput(div, 'number', "Populate from zoom", 'injectZoom');
   addInput(div, 'checkbox', "Store team", 'storeTeam');
-  addInput(div, 'number', "Hours to keep data", 'maxAge');
-
-  section = L.DomUtil.create('h4', '', div);
-  section.textContent = "Cache for location history";
-  section.style.gridColumn = "1/3";
   addInput(div, 'number', "Days to keep data", 'maxLocationAge');
 
   window.dialog({
@@ -337,79 +261,8 @@ function displayOpt() {
     html: div,
     width: 'auto',
     buttons: {
-      'History': displayHistory
+      'History': () => alert("Portal history has been removed. Prefer using https://github.com/vrabcak/iitc-offle instead.")
     }
-  });
-}
-
-function displayHistory() {
-  if (!cachePortals.db) return;
-  var div = L.DomUtil.create('div');
-
-  L.DomUtil.create("h4", '', div).textContent = "Portals with multiple locations";
-
-  var guidSelect = L.DomUtil.create('select', '', div);
-  guidSelect.innerHTML = "<option selected disabled>Select a guid</option>"
-  var historySelect = L.DomUtil.create('select', '', div);
-  var pre = L.DomUtil.create('pre', '', div);
-  var curHist = {};
-
-  L.DomEvent.on(historySelect, 'change', () => {
-    var p = curHist[historySelect.value];
-    if (!p) return;
-    pre.textContent = `Portal ${p.guid}
-First seen: ${new Date(p.date).toLocaleString()}
-Last seen: ${new Date(p.lastSeen).toLocaleString()}
-Lat: ${p.latE6/1e6}
-Lng: ${p.lngE6/1e6}`;
-  });
-
-  L.DomEvent.on(guidSelect, 'change', function (ev) {
-    var guid = guidSelect.value;
-    historySelect.innerHTML = "<option selected disabled>Select a date</option>";
-    curHist = {};
-    var tx = cachePortals.db.transaction("portals_history", "readonly");
-    var portalsStore = tx.objectStore("portals_history");
-    var guidIndex = portalsStore.index("guid");
-    guidIndex.getAll(guid).onsuccess = function (e) {
-      var portals = e.target.result;
-      portals.forEach((p) => {
-        curHist[p.date] = p;
-        var option = L.DomUtil.create('option', '', historySelect);
-        option.value = p.date;
-        option.textContent = new Date(p.date).toLocaleString();
-      })
-    }
-  });
-
-  var tx = cachePortals.db.transaction("portals_history", "readonly");
-  var portalsStore = tx.objectStore("portals_history");
-  var guidIndex = portalsStore.index("guid");
-
-  var lastGuid = null;
-  guidIndex.openKeyCursor().onsuccess = function (e) {
-    var cursor = e.target.result;
-    if (cursor) {
-      var guid = cursor.key;
-      if (lastGuid === guid) {
-        var opt = L.DomUtil.create('option', '', guidSelect)
-        opt.textContent = guid;
-        cursor.continue(guid + 'x');
-      } else {
-        lastGuid = guid;
-        cursor.continue();
-      }
-    }
-  };
-
-  tx.onerror = function (e) {
-    console.error(e);
-  };
-
-  window.dialog({
-    title: "Portal Location History",
-    html: div,
-    width: 'auto',
   });
 }
 
@@ -419,6 +272,7 @@ function setup() {
 
   try {
     var settings = JSON.parse(localStorage[cachePortals.SETTINGS_KEY]);
+    delete settings.maxAge;
     Object.assign(cachePortals.settings, settings);
   } catch {
     saveSettings();
@@ -431,7 +285,7 @@ function setup() {
   window.addHook("mapDataEntityInject", entityInject);
 
   $('<a>')
-    .html('Portal Cache')
+    .html('Portal Cache Opt')
     .click(displayOpt)
     .appendTo('#toolbox');
 }
