a153ccf64880af39ae1abaa6ee4b9696d53e3200
LeJeu <64744459+le-jeu@users.noreply.github.com>
2021-10-14 14:49:58 +0200

    remove CR+tab

--- plugins/localstorage-manager.js
+++ plugins/localstorage-manager.js
@@ -13,177 +13,177 @@
 var manageStorage = {};
 window.plugin.manageStorage = manageStorage;
 
-	function generateFile(type, filename, text){
-		var pom = document.createElement('a');
-		pom.setAttribute('href', type + encodeURIComponent(text));
-		pom.setAttribute('download', filename);
+function generateFile(type, filename, text){
+	var pom = document.createElement('a');
+	pom.setAttribute('href', type + encodeURIComponent(text));
+	pom.setAttribute('download', filename);
 
-		pom.style.display = 'none';
-		document.body.appendChild(pom);
+	pom.style.display = 'none';
+	document.body.appendChild(pom);
 
-		pom.click();
-		document.body.removeChild(pom);
+	pom.click();
+	document.body.removeChild(pom);
+}
+
+function importFile(){
+	var fileInput = document.getElementById('manage-localstorage-import');
+	var file = fileInput.files[0];
+	var textType = /text.*/;
+
+	if (file.type.match(textType)){
+		var reader = new FileReader();
+
+		reader.onload = function(e){
+			window.newData = JSON.parse(reader.result);
+
+			for(storageName in newData){
+				window.localStorage[storageName] = newData[storageName];
+			}
+		}
+		reader.readAsText(file);
+		alert('Import successful! Refresh the page.');
+	}else{
+		alert('File not supported!');
+	}
+}
+
+function createFile(){
+	var type = 'data:text/plain;charset=utf-8,';
+	var selectedStorage = getSelectedKey();
+	var title = 'localstorage-'+selectedStorage+'-'+new Date().getTime();
+	if(title == ''){ title = 'untitled'; }
+	var filename = title+'.txt';
+
+	var str = {};
+
+	var store = window.localStorage[selectedStorage];
+
+	var text = '{"'+selectedStorage+'":'+JSON.stringify(store)+'}';
+
+	generateFile(type, filename, text);
+}
+
+function importData(){
+	window.dialog({
+		html: '<div class="local-storage-import"><p>Selected a single or global data file. <input type="file" id="manage-localstorage-import" accept="text/plain" onchange="window.plugin.manageStorage.importFile();return false;" /></div>',
+		dialogClass: 'ui-dialog-local-storage-import',
+		title: 'LocalStorage Manager - Import'
+	});
+}
+
+function reset(){
+	var KEY = getSelectedKey();
+	var promptAction = prompt('"'+KEY+'" data will be deleted. Are you sure? [Y/N]', '');
+	if(promptAction !== null && (promptAction === 'Y' || promptAction === 'y')){
+		delete localStorage[KEY];
+		$('a.localData:contains(\''+KEY+'\')').remove();
+	}
+}
+
+function createFileAll(){
+	var type = 'data:text/plain;charset=utf-8,';
+	var title = 'localstorage-ALL-'+new Date().getTime();
+	if(title == ''){ title = 'untitled'; }
+	var filename = title+'.txt';
+
+	var text = JSON.stringify(window.localStorage);
+
+	generateFile(type, filename, text);
+}
+
+function resetAll(){
+	var promptAction = prompt('WARNING: All localStorage will be deleted. Are you sure? [Y/N]', '');
+	if(promptAction !== null && (promptAction === 'Y' || promptAction === 'y')){
+		var promptAction2 = prompt('WARNING: all, all, all your localStorage will be deleted. Are you really really really sure? [Y/N]', '');
+		if(promptAction2 !== null && (promptAction2 === 'Y' || promptAction2 === 'y')){
+			localStorage.clear();
+			$('a.localData').remove();
+		}
+	}
+}
+
+function selectKey(elem){
+	$('.manage-local-storage a.localData').removeClass('selected');
+	$(elem).addClass('selected');
+}
+function getSelectedKey(){
+	return $('.manage-local-storage a.localData.selected').data('key')
+}
+
+function setupContent(){
+	var txt = '';
+
+	var items = [];
+
+	for(var i = 0; i < localStorage.length; i++) {
+		var key = localStorage.key(i);
+		var size = localStorage.getItem(key).length;
+		items.push([key, size]);
 	}
 
-	function importFile(){
-		var fileInput = document.getElementById('manage-localstorage-import');
-		var file = fileInput.files[0];
-		var textType = /text.*/;
+	items.sort((a,b) => b[1] - a[1]);
 
-		if (file.type.match(textType)){
-			var reader = new FileReader();
+	for (var i=0; i < items.length; i++) {
+		txt += '<a class="localData" onclick="window.plugin.manageStorage.selectKey(this);return false;" data-key="' + items[i][0] + '">'+items[i][0]+' (' + items[i][1].toLocaleString() +')</a>';
+	}
 
-			reader.onload = function(e){
-				window.newData = JSON.parse(reader.result);
+	var warn = '- Don\'t import with this plugin data exported by a different plugin.<br/>';
+	warn += '- Don\'t import the data exported by a different plugin with this plugin.';
 
-				for(storageName in newData){
-					window.localStorage[storageName] = newData[storageName];
+	return warn+'<div class="manage-local-storage">'+txt+'</div>';
+}
+
+function dialog(){
+	window.dialog({
+		html: setupContent,
+		dialogClass: 'ui-dialog-local-storage',
+		title: 'LocalStorage Manager',
+		buttons:{
+			'EXPORT': function(){
+				if($('.manage-local-storage a.localData.selected').length){
+					createFile();
 				}
-			}
-			reader.readAsText(file);
-			alert('Import successful! Refresh the page.');
-		}else{
-			alert('File not supported!');
-		}
-	}
-
-	function createFile(){
-		var type = 'data:text/plain;charset=utf-8,';
-		var selectedStorage = getSelectedKey();
-		var title = 'localstorage-'+selectedStorage+'-'+new Date().getTime();
-		if(title == ''){ title = 'untitled'; }
-		var filename = title+'.txt';
-
-		var str = {};
-
-		var store = window.localStorage[selectedStorage];
-
-		var text = '{"'+selectedStorage+'":'+JSON.stringify(store)+'}';
-
-		generateFile(type, filename, text);
-	}
-
-	function importData(){
-		window.dialog({
-			html: '<div class="local-storage-import"><p>Selected a single or global data file. <input type="file" id="manage-localstorage-import" accept="text/plain" onchange="window.plugin.manageStorage.importFile();return false;" /></div>',
-			dialogClass: 'ui-dialog-local-storage-import',
-			title: 'LocalStorage Manager - Import'
-		});
-	}
-
-	function reset(){
-		var KEY = getSelectedKey();
-		var promptAction = prompt('"'+KEY+'" data will be deleted. Are you sure? [Y/N]', '');
-		if(promptAction !== null && (promptAction === 'Y' || promptAction === 'y')){
-			delete localStorage[KEY];
-			$('a.localData:contains(\''+KEY+'\')').remove();
-		}
-	}
-
-	function createFileAll(){
-		var type = 'data:text/plain;charset=utf-8,';
-		var title = 'localstorage-ALL-'+new Date().getTime();
-		if(title == ''){ title = 'untitled'; }
-		var filename = title+'.txt';
-
-		var text = JSON.stringify(window.localStorage);
-
-		generateFile(type, filename, text);
-	}
-
-	function resetAll(){
-		var promptAction = prompt('WARNING: All localStorage will be deleted. Are you sure? [Y/N]', '');
-		if(promptAction !== null && (promptAction === 'Y' || promptAction === 'y')){
-			var promptAction2 = prompt('WARNING: all, all, all your localStorage will be deleted. Are you really really really sure? [Y/N]', '');
-			if(promptAction2 !== null && (promptAction2 === 'Y' || promptAction2 === 'y')){
-				localStorage.clear();
-				$('a.localData').remove();
-			}
-		}
-	}
-
-	function selectKey(elem){
-		$('.manage-local-storage a.localData').removeClass('selected');
-		$(elem).addClass('selected');
-	}
-	function getSelectedKey(){
-		return $('.manage-local-storage a.localData.selected').data('key')
-	}
-
-	function setupContent(){
-		var txt = '';
-
-		var items = [];
-
-		for(var i = 0; i < localStorage.length; i++) {
-			var key = localStorage.key(i);
-			var size = localStorage.getItem(key).length;
-			items.push([key, size]);
-		}
-
-		items.sort((a,b) => b[1] - a[1]);
-
-		for (var i=0; i < items.length; i++) {
-			txt += '<a class="localData" onclick="window.plugin.manageStorage.selectKey(this);return false;" data-key="' + items[i][0] + '">'+items[i][0]+' (' + items[i][1].toLocaleString() +')</a>';
-		}
-
-		var warn = '- Don\'t import with this plugin data exported by a different plugin.<br/>';
-		warn += '- Don\'t import the data exported by a different plugin with this plugin.';
-
-		return warn+'<div class="manage-local-storage">'+txt+'</div>';
-	}
-
-	function dialog(){
-		window.dialog({
-			html: setupContent,
-			dialogClass: 'ui-dialog-local-storage',
-			title: 'LocalStorage Manager',
-			buttons:{
-				'EXPORT': function(){
-					if($('.manage-local-storage a.localData.selected').length){
-						createFile();
-					}
-				},
-				'DELETE': function(){
-					if($('.manage-local-storage a.localData.selected').length){
-						reset();
-					}
-				},
-				'IMPORT': function(){
-						importData();
-				},
-				'EXPORT ALL': function(){
-					createFileAll();
-				},
-				'DELETE ALL': function(){
-					resetAll();
+			},
+			'DELETE': function(){
+				if($('.manage-local-storage a.localData.selected').length){
+					reset();
 				}
+			},
+			'IMPORT': function(){
+					importData();
+			},
+			'EXPORT ALL': function(){
+				createFileAll();
+			},
+			'DELETE ALL': function(){
+				resetAll();
 			}
-		});
-		$('.ui-dialog-local-storage button:contains(\'DELETE ALL\')').addClass('warning');
-	}
+		}
+	});
+	$('.ui-dialog-local-storage button:contains(\'DELETE ALL\')').addClass('warning');
+}
 
-	function setupCSS(){
-		$('<style>').prop('type', 'text/css').html(''
-			+'.manage-local-storage .localData{display:block; border:1px solid #aaa; color:#aaa; padding:4px 4px 2px; text-align:center; margin:7px 2px;}'
-			+'.manage-local-storage .localData:hover{text-decoration:none;background:rgba(8,48,78,.85);}'
-			+'.manage-local-storage .localData.selected,#manage-localstorage-import{border:1px solid #ffce00;color:#ffce00;background:rgba(8,48,78,.85);}'
-			+'.ui-dialog-local-storage button{margin:4px 0;margin-left:6px;padding: 5px 5px 4px;}'
-			+'.ui-dialog-local-storage button.warning{border-color:#FF5454; color:#FF5454;}'
-			+'.local-storage-import #manage-localstorage-import{margin-top:10px;}'
-		).appendTo('head');
-	}
+function setupCSS(){
+	$('<style>').prop('type', 'text/css').html(''
+		+'.manage-local-storage .localData{display:block; border:1px solid #aaa; color:#aaa; padding:4px 4px 2px; text-align:center; margin:7px 2px;}'
+		+'.manage-local-storage .localData:hover{text-decoration:none;background:rgba(8,48,78,.85);}'
+		+'.manage-local-storage .localData.selected,#manage-localstorage-import{border:1px solid #ffce00;color:#ffce00;background:rgba(8,48,78,.85);}'
+		+'.ui-dialog-local-storage button{margin:4px 0;margin-left:6px;padding: 5px 5px 4px;}'
+		+'.ui-dialog-local-storage button.warning{border-color:#FF5454; color:#FF5454;}'
+		+'.local-storage-import #manage-localstorage-import{margin-top:10px;}'
+	).appendTo('head');
+}
 
-	var setup = function(){
-		setupCSS();
+var setup = function(){
+	setupCSS();
 
-		/* expose api for click event */
-		manageStorage.selectKey = selectKey;
-		manageStorage.importFile = importFile;
-		manageStorage.dialog = dialog;
+	/* expose api for click event */
+	manageStorage.selectKey = selectKey;
+	manageStorage.importFile = importFile;
+	manageStorage.dialog = dialog;
 
-		$('#toolbox').append('<a class="list-group-item" onclick="window.plugin.manageStorage.dialog();return false;" title="Manage Storage"><i class="fa fa-hdd-o"></i>LocalStorage</a>');
-	}
+	$('#toolbox').append('<a class="list-group-item" onclick="window.plugin.manageStorage.dialog();return false;" title="Manage Storage"><i class="fa fa-hdd-o"></i>LocalStorage</a>');
+}
 
 // PLUGIN END //////////////////////////////////////////////////////////
 
