4e49dd42b22349e97115d80785b1d6d025f89ca2
LeJeu <64744459+le-jeu@users.noreply.github.com>
2022-05-04 11:58:17 +0200

    add search by guid plugin

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ import metablock from "./rollup-plugin-iitcplugin";
 const buildPath = "dist";
 const pluginsPath = "src";
 
-let pluginsId = [];
+let pluginsId = ["search-guid"];
 
 export default pluginsId.map((p) => ({
   input: path.join(pluginsPath, p, "index.js"),
