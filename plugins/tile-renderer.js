8a154d4daaecc07cddff92d27dd0f36fd47d5114
LeJeu <64744459+le-jeu@users.noreply.github.com>
2022-07-14 17:15:57 +0200

    handle future ornaments

--- plugins/tile-renderer.js
+++ plugins/tile-renderer.js
@@ -1,7 +1,7 @@
 // @author         jaiperdu
 // @name           Tile rendering using canvas
 // @category       Tweaks
-// @version        0.2.1
+// @version        0.2.2
 // @description    Render vector layers and ornaments with canvas tiles
 
 function setup() {
@@ -92,6 +92,79 @@ function setup() {
     });
   }
 
+  function getOrnamentLayer(ornament) {
+    var ornaments = window.ornaments;
+    if (ornaments.createLayer) {
+      var layer = ornaments.layers["Ornaments"];
+
+      if (!ornaments.knownOrnaments[ornament]) {
+        ornaments.knownOrnaments[ornament] = false;
+      }
+
+      if (ornament in ornaments.icon) {
+        if (ornaments.icon[ornament].layer) {
+          if (ornaments.layers[ornaments.icon[ornament].layer] === undefined) {
+            window.ornaments.createLayer(window.ornaments.icon[ornament].layer);
+          }
+          layer = ornaments.layers[window.ornaments.icon[ornament].layer];
+        }
+      }
+
+      var exclude = false;
+      if (ornaments.excludedOrnaments && ornaments.excludedOrnaments !== [""]) {
+        exclude = ornaments.excludedOrnaments.some(function (pattern) {
+          return ornament.startsWith(pattern);
+        });
+      }
+      exclude = exclude | ornaments.knownOrnaments[ornament];
+      if (exclude) {
+        layer = ornaments.layers["Excluded ornaments"];
+      }
+      return layer;
+    }
+
+    var layer = ornaments._layer;
+    if (ornament.startsWith("pe")) {
+      layer = ornament === "peFRACK" ? ornaments._frackers : ornaments._beacons;
+    }
+    return layer;
+  }
+
+  function getOrnamentIcon(ornament) {
+    var size = window.ornaments.OVERLAY_SIZE * window.portalMarkerScale();
+    var anchor = [size / 2, size / 2];
+    var iconUrl = '//commondatastorage.googleapis.com/ingress.com/img/map_icons/marker_images/' + ornament + '.png';
+
+    if (window.ornaments.createLayer) {
+      var ornaments = window.ornaments;
+      if (ornament in ornaments.icon) {
+        if (ornaments.icon[ornament].url) {
+          iconUrl = ornaments.icon[ornament].url;
+          if (ornaments.icon[ornament].offset) {
+            var offset = ornaments.icon[ornament].offset;
+            anchor = [size * offset[0] + anchor[0], size * offset[1] + anchor[1]];
+          }
+        }
+      }
+    }
+    return L.icon({
+      iconUrl: iconUrl,
+      iconSize: [size, size],
+      iconAnchor: anchor,
+    });
+  }
+
+  function addOrnament(portal, ornament) {
+    var layer = getOrnamentLayer(ornament);
+    var icon = getOrnamentIcon(ornament);
+    var marker = canvasMarker(portal.getLatLng(), {
+      icon: icon,
+      interactive: false,
+      layer: layer,
+    });
+    return marker.addTo(layer);
+  }
+
   // use the tilecanvas renderer for ornaments too
   window.ornaments.addPortal = function (portal) {
     this.removePortal(portal);
@@ -99,25 +172,7 @@ function setup() {
     var ornaments = portal.options.data.ornaments;
     if (ornaments && ornaments.length) {
       this._portals[portal.options.guid] = ornaments.map(function (ornament) {
-        var layer = this._layer;
-        if (ornament.startsWith("pe")) {
-          layer = ornament === "peFRACK" ? this._frackers : this._beacons;
-        }
-        var size = this.OVERLAY_SIZE;
-        return canvasMarker(portal.getLatLng(), {
-          icon: L.icon({
-            iconUrl:
-              "//commondatastorage.googleapis.com/ingress.com/img/map_icons/marker_images/" +
-              ornament +
-              ".png",
-            iconSize: [size, size],
-            iconAnchor: [size / 2, size / 2],
-          }),
-          interactive: false,
-          keyboard: false,
-          opacity: this.OVERLAY_OPACITY,
-          layer: layer,
-        }).addTo(layer);
+        return addOrnament(portal, ornament);
       }, this);
     }
   };
