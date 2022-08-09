d839e87f4fb2cb7694ec2be88c870bf50ce9dfc8
LeJeu <64744459+le-jeu@users.noreply.github.com>
2022-08-09 12:35:58 +0200

    use rgb mapbox-like style

--- plugins/highlight-customize.js
+++ plugins/highlight-customize.js
@@ -7,10 +7,47 @@
 const customHighlight = {};
 // const STORAGE_KEY = 'plugin-highlight-customized';
 
+function clamp(a, min, max) {
+  if (a < min) return min;
+  if (a > max) return max;
+  return a;
+}
+
+class Color {
+  constructor(rgb) {
+    this.array = rgb;
+  }
+  toString() {
+    return (
+      '#' +
+      this.array
+        .map((v) => clamp(v, 0, 255))
+        .map((v) => Math.round(v).toString(16))
+        .map((s) => s.slice(-2).padStart(2, '0'))
+        .join('')
+    );
+  }
+}
+
+/** Retrieve the value of the property `prop` of the portal */
+function getExpr(expr, portal) {
+  const prop = expr[1];
+  if (prop in portal.options) {
+    return portal.options[prop];
+  }
+  if (prop in portal.options.data) {
+    return portal.options.data[prop];
+  }
+  if (portal.options.data.history && prop in portal.options.data.history) {
+    return portal.options.data.history[prop];
+  }
+  return null;
+}
+
 function compareExpr(expr, portal) {
   const [op, e1, e2] = expr;
-  const v1 = genericExpr(e1, portal);
-  const v2 = genericExpr(e2, portal);
+  const v1 = evaluateExpr(e1, portal);
+  const v2 = evaluateExpr(e2, portal);
   switch (op) {
     case '==':
       return v1 === v2;
@@ -28,96 +65,64 @@ function compareExpr(expr, portal) {
   return false;
 }
 
-function genericBoolean(expr, portal) {
-  if (typeof expr === 'boolean') return expr;
-  if (expr instanceof Array) {
-    const op = expr[0];
-    switch (op) {
-      case 'any':
-      case 'or':
-        for (let i = 1; i < expr.length; i++) {
-          if (genericBoolean(expr[i], portal)) {
-            return true;
-          }
+function booleanExpr(expr, portal) {
+  const op = expr[0];
+  switch (op) {
+    case 'any':
+    case 'or':
+      for (let i = 1; i < expr.length; i++) {
+        if (evaluateExpr(expr[i], portal)) {
+          return true;
         }
-        return false;
-      case 'all':
-      case 'and':
-        for (let i = 1; i < expr.length; i++) {
-          if (!genericBoolean(expr[i], portal)) {
-            return false;
-          }
-        }
-        return true;
-      case 'not':
-      case '!':
-        return !genericBoolean(expr[1], portal);
-      case '==':
-      case '!=':
-      case '<':
-      case '<=':
-      case '>':
-      case '>=':
-        return compareExpr(expr, portal);
-      case 'in': {
-        const v = genericExpr(expr[1], portal);
-        const a = genericExpr(expr[2], portal);
-        return a && !!a.find((a) => a === v);
       }
-      case 'get':
-        return getExpr(expr[1], portal);
-      default:
-      // unknown op
+      return false;
+    case 'all':
+    case 'and':
+      for (let i = 1; i < expr.length; i++) {
+        if (!evaluateExpr(expr[i], portal)) {
+          return false;
+        }
+      }
+      return true;
+    case 'not':
+    case '!':
+      return !evaluateExpr(expr[1], portal);
+    case 'in': {
+      const v = evaluateExpr(expr[1], portal);
+      const a = evaluateExpr(expr[2], portal);
+      return a && !!a.find((a) => a === v);
     }
-    return false;
+    default:
+    // unknown op
   }
   return false;
 }
 
-function getExpr(prop, portal) {
-  if (prop in portal.options) {
-    return portal.options[prop];
-  }
-  if (prop in portal.options.data) {
-    return portal.options.data[prop];
-  }
-  if (portal.options.data.history && prop in portal.options.data.history) {
-    return portal.options.data.history[prop];
-  }
-  return null;
-}
-
-function clamp(a, min, max) {
-  if (a < min) return min;
-  if (a > max) return max;
-  return a;
-}
-
 function caseExpr(expr, portal) {
   for (let i = 1; i < expr.length - 1; i += 2) {
-    if (genericBoolean(expr[i], portal)) return genericExpr(expr[i + 1], portal);
+    if (evaluateExpr(expr[i], portal)) return evaluateExpr(expr[i + 1], portal);
   }
-  return genericExpr(expr[expr.length - 1], portal);
+  return evaluateExpr(expr[expr.length - 1], portal);
 }
 
 function matchExpr(expr, portal) {
-  const v = genericExpr(expr[1], portal);
+  const v = evaluateExpr(expr[1], portal);
   for (let i = 2; i < expr.length - 1; i += 2) {
-    if (genericExpr(expr[i], portal) === v) return genericExpr(expr[i + 1], portal);
+    if (evaluateExpr(expr[i], portal) === v) return evaluateExpr(expr[i + 1], portal);
   }
-  return genericExpr(expr[expr.length - 1], portal);
+  return evaluateExpr(expr[expr.length - 1], portal);
 }
 
 function interpolateExpr(expr, portal) {
-  // assume expr[1] == 'linear'
-  const v = genericExpr(expr[2], portal);
-  if (v <= expr[3]) return genericExpr(expr[4], portal);
-  if (expr[expr.length - 2] < v) return genericExpr(expr[expr.length - 1], portal);
+  // assume expr[1] == ['linear']
+  const v = evaluateExpr(expr[2], portal);
+  if (v <= expr[3]) return evaluateExpr(expr[4], portal);
+  if (expr[expr.length - 2] < v) return evaluateExpr(expr[expr.length - 1], portal);
   let index = 5;
   while (index < expr.length && expr[index] < v) index += 2;
   // last item
-  const subValue = genericExpr(expr[index - 1], portal);
-  const supValue = genericExpr(expr[index + 1], portal);
+  const subValue = evaluateExpr(expr[index - 1], portal);
+  const supValue = evaluateExpr(expr[index + 1], portal);
   return interpolate(v, expr[index - 2], expr[index], subValue, supValue);
 }
 
@@ -132,37 +137,94 @@ function interpolate(v, inMin, inMax, outMin, outMax) {
     }
     return ret;
   }
+  if (outMin instanceof Color) {
+    return new Color(interpolate(v, inMin, inMax, outMin.array, outMax.array));
+  }
   // error?
   return 0;
 }
 
 function rgbExpr(expr, portal) {
-  const components = genericExpr(expr[1], portal)
-    .map((v) => clamp(v, 0, 255))
-    .map((v) => Math.round(v).toString(16))
-    .map((s) => s.slice(-2).padStart(2, '0'))
-    .join('');
-  return '#' + components;
+  return new Color([evaluateExpr(expr[1], portal), evaluateExpr(expr[2], portal), evaluateExpr(expr[3], portal)]);
+}
+
+function sliceExpr(expr, portal) {
+  const a = evaluateExpr(expr[1], portal);
+  const s = evaluateExpr(expr[2], portal);
+  return a.slice(s);
+}
+
+function mathExpr(expr, portal) {
+  const op = expr[0];
+  if (op === '+') {
+    let ret = 0;
+    for (let i = 1; i < expr.length; i++) {
+      ret += evaluateExpr(expr[i], portal);
+    }
+    return ret;
+  }
+  if (op === '*') {
+    let ret = 1;
+    for (let i = 1; i < expr.length; i++) {
+      ret *= evaluateExpr(expr[i], portal);
+    }
+    return ret;
+  }
+  const v1 = evaluateExpr(expr[1], portal);
+  const v2 = evaluateExpr(expr[2], portal);
+  switch (op) {
+    case '-':
+      return v1 - v2;
+    case '/':
+      return v1 / v2;
+    case '%':
+      return v1 % v2;
+    case '^':
+      return v1 ^ v2;
+  }
+  return null;
+}
+
+function literalExpr(expr) {
+  return expr[1];
+}
+
+function zoomExpr() {
+  return window.map.getZoom();
 }
 
 customHighlight.operators = {
-  get: (expr, portal) => getExpr(expr[1], portal),
-  literal: (expr) => expr[1],
-  '+': (expr, portal) => expr.slice(1).reduce((a, b) => a + +genericExpr(b, portal), 0),
-  '*': (expr, portal) => expr.slice(1).reduce((a, b) => a * +genericExpr(b, portal), 1),
-  '-': (expr, portal) => +genericExpr(expr[1], portal) - +genericExpr(expr[2], portal),
-  '/': (expr, portal) => +genericExpr(expr[1], portal) / +genericExpr(expr[2], portal),
-  '%': (expr, portal) => +genericExpr(expr[1], portal) % +genericExpr(expr[2], portal),
+  get: getExpr,
+  literal: literalExpr,
+  '+': mathExpr,
+  '*': mathExpr,
+  '-': mathExpr,
+  '/': mathExpr,
+  '%': mathExpr,
+  '^': mathExpr,
+  '==': compareExpr,
+  '!=': compareExpr,
+  '<': compareExpr,
+  '<=': compareExpr,
+  '>': compareExpr,
+  '>=': compareExpr,
+  or: booleanExpr,
+  and: booleanExpr,
+  any: booleanExpr,
+  all: booleanExpr,
+  not: booleanExpr,
+  '!': booleanExpr,
+  in: booleanExpr,
   match: matchExpr,
   case: caseExpr,
   interpolate: interpolateExpr,
-  rgb: rgbExpr, // note: this is different from mapbox style,
-  slice: (expr, portal) => genericExpr(expr[1], portal).slice(genericExpr(expr[2], portal)),
-  now: () => Date.now(),
-  zoom: () => window.map.getZoom(),
+  rgb: rgbExpr,
+  slice: sliceExpr,
+  now: Date.now,
+  zoom: zoomExpr,
 };
 
-function genericExpr(expr, portal) {
+function evaluateExpr(expr, portal) {
   if (expr instanceof Array) {
     const operator = expr[0];
     if (operator in customHighlight.operators) {
@@ -176,8 +238,11 @@ function genericExpr(expr, portal) {
 function computeStyle(style, portal) {
   const res = {};
   for (const key in style) {
-    const v = genericExpr(style[key], portal);
-    if (v !== null) res[key] = v;
+    const v = evaluateExpr(style[key], portal);
+    if (v !== null) {
+      if (v instanceof Color) res[key] = v.toString();
+      else res[key] = v;
+    }
   }
   return res;
 }
@@ -185,8 +250,8 @@ function computeStyle(style, portal) {
 customHighlight.styles = {
   // examples, this needs a UI to define user based.
   rainbow: {
-    color: ['rgb', ['interpolate', 'linear', ['get', 'level'], 0, ['literal', [255, 0, 0]], 4, ['literal', [0, 255, 0]], 8, ['literal', [0, 0, 255]]]],
-    fillColor: ['rgb', ['interpolate', 'linear', ['get', 'level'], 0, ['literal', [255, 0, 0]], 4, ['literal', [0, 255, 0]], 8, ['literal', [0, 0, 255]]]],
+    color: ['interpolate', ['linear'], ['get', 'level'], 0, ['rgb', 255, 0, 0], 4, ['rgb', 0, 255, 0], 8, ['rgb', 0, 0, 255]],
+    fillColor: ['interpolate', ['linear'], ['get', 'level'], 0, ['rgb', 255, 0, 0], 4, ['rgb', 0, 255, 0], 8, ['rgb', 0, 0, 255]],
   },
   transparentCaptured: {
     fillOpacity: ['case', ['get', 'captured'], 0, 0.6],
@@ -198,7 +263,7 @@ customHighlight.styles = {
     fillOpacity: [
       'case',
       ['and', ['!=', ['get', 'team'], 0], ['<', ['get', 'resCount'], 8]],
-      ['interpolate', 'linear', ['get', 'resCount'], 0, 1, 8, 0.15],
+      ['interpolate', ['linear'], ['get', 'resCount'], 0, 1, 8, 0.15],
       null,
     ],
     dashArray: [
@@ -209,13 +274,53 @@ customHighlight.styles = {
     ],
     fillColor: ['case', ['and', ['!=', ['get', 'team'], 0], ['<', ['get', 'resCount'], 8]], 'red', null],
   },
+  older: {
+    color: [
+      'interpolate',
+      ['linear'],
+      ['-', ['now'], ['get', 'timestamp']],
+      0,
+      ['rgb', 255, 0, 0],
+      17280000,
+      ['rgb', 255, 255, 0],
+      34560000,
+      ['rgb', 0, 255, 0],
+      51840000,
+      ['rgb', 0, 255, 255],
+      69120000,
+      ['rgb', 0, 0, 255],
+      86400000,
+      ['rgb', 255, 0, 255],
+      86400001,
+      ['rgb', 128, 128, 128],
+    ],
+    fillColor: [
+      'interpolate',
+      ['linear'],
+      ['-', ['now'], ['get', 'timestamp']],
+      0,
+      ['rgb', 255, 0, 0],
+      17280000,
+      ['rgb', 255, 255, 0],
+      34560000,
+      ['rgb', 0, 255, 0],
+      51840000,
+      ['rgb', 0, 255, 255],
+      69120000,
+      ['rgb', 0, 0, 255],
+      86400000,
+      ['rgb', 255, 0, 255],
+      86400001,
+      ['rgb', 128, 128, 128],
+    ],
+  },
 };
 
 /* exported setup */
 function setup() {
   window.addPortalHighlighter('Custom Highlighter', function (data) {
     const portal = data.portal;
-    const style = computeStyle(customHighlight.styles.missingResonators, portal);
+    const style = computeStyle(customHighlight.styles.older, portal);
     portal.setStyle(style);
   });
 }
