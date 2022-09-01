f8bd6be2e99ed236053f7d6c8d06c06e80647824
LeJeu <64744459+le-jeu@users.noreply.github.com>
2022-09-01 22:48:57 +0200

    custom layout for summary

--- src/player-inventory/index.jsx
+++ src/player-inventory/index.jsx
@@ -78,48 +78,172 @@ function createAllTable(inventory) {
   return table;
 }
 
-function keysSum(inventory) {
+function createAllSumTable(inventory) {
   const total = inventory.items['PORTAL_LINK_KEY'].total;
   const inventoryCount = inventory.items['PORTAL_LINK_KEY'].counts['VERY_COMMON'][inventory.name] || 0;
   const otherCount = total - inventoryCount - inventory.keyLockersCount;
-  return (
-    <>
-      <span>
-        {inventory.name}: {inventoryCount}
-      </span>
-      <span>Key Lockers: {inventory.keyLockersCount}</span>
-      <span>Other: {otherCount}</span>
-    </>
-  );
-}
-
-function itemSum(item) {
-  return Object.keys(item.counts).map((k) => {
-    const lr = item.leveled ? 'L' + k : rarityShort[rarityToInt[k]];
-    return (
-      <span className={(item.leveled ? 'level_' : 'rarity_') + lr}>
-        {item.counts[k].total} {lr}
-      </span>
-    );
-  });
-}
-
-function createAllSumTable(inventory) {
-  const table = <table></table>;
+  let beacon = 0;
   for (const type in inventory.items) {
-    const total = inventory.countType(type);
-    if (total === 0) continue;
-    const item = inventory.items[type];
-
-    table.append(
-      <tr>
-        <td>{item.name}</td>
-        <td>{total}</td>
-        {type === 'PORTAL_LINK_KEY' ? <td className="level_L1">{keysSum(inventory)}</td> : <td>{itemSum(item)}</td>}
-      </tr>
-    );
+    if (type.startsWith('PORTAL_POWERUP')) {
+      switch (type) {
+        case 'PORTAL_POWERUP:FRACK':
+        case 'PORTAL_POWERUP:BB_BATTLE_RARE':
+        case 'PORTAL_POWERUP:BB_BATTLE':
+        case 'PORTAL_POWERUP:FW_ENL':
+        case 'PORTAL_POWERUP:FW_RES':
+          break;
+        default:
+          beacon++;
+      }
+    }
   }
-  return table;
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Portal Keys</th>
+          <th>⌂</th>
+          <th>Lockers</th>
+          <th>Other</th>
+        </tr>
+        <tr>
+          <th>{total}</th>
+          <td>{inventoryCount}</td>
+          <td>{inventory.keyLockersCount}</td>
+          <td>{otherCount}</td>
+        </tr>
+      </table>
+      <table>
+        {[
+          ['EMITTER_A', 'R'],
+          ['EMP_BURSTER', 'B'],
+          ['ULTRA_STRIKE', 'US'],
+          ['POWER_CUBE', 'PC'],
+        ].map(([type, short]) => (
+          <>
+            <tr>
+              <th>{itemTypes[type]}</th>
+              {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
+                <th className={'level_L' + i}>{short + i}</th>
+              ))}
+            </tr>
+            <tr>
+              <th>{inventory.countType(type)}</th>
+              {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
+                <td>{inventory.countType(type, i)}</td>
+              ))}
+            </tr>
+          </>
+        ))}
+      </table>
+      <table>
+        <tr>
+          <th>Hypercube</th>
+          <th>ADA Refactor</th>
+          <th>JARVIS Virus</th>
+        </tr>
+        <tr>
+          <td>{inventory.countType('BOOSTED_POWER_CUBE') + inventory.countType('BOOSTED_POWER_CUBE_K')}</td>
+          <td>{inventory.countType('FLIP_CARD:ADA')}</td>
+          <td>{inventory.countType('FLIP_CARD:JARVIS')}</td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <th>Shield</th>
+          <th className="rarity_C">C</th>
+          <th className="rarity_R">R</th>
+          <th className="rarity_VR">VR</th>
+          <th className="rarity_VR">Aegis</th>
+        </tr>
+        <tr>
+          <th>{inventory.countType('RES_SHIELD') + inventory.countType('EXTRA_SHIELD')}</th>
+          {['COMMON', 'RARE', 'VERY_RARE'].map((k) => (
+            <td>{inventory.countType('RES_SHIELD', k)}</td>
+          ))}
+          <td>{inventory.countType('EXTRA_SHIELD')}</td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <th>Turret</th>
+          <th>Force Amp</th>
+          <th>Link Amp</th>
+          {inventory.countType('LINK_AMPLIFIER', 'VERY_RARE') ? <th className="rarity_VR">LA VR</th> : null}
+          <th>Ultra Link</th>
+          <th>ITO +</th>
+          <th>ITO -</th>
+        </tr>
+        <tr>
+          <td>{inventory.countType('TURRET')}</td>
+          <td>{inventory.countType('FORCE_AMP')}</td>
+          <td>{inventory.countType('LINK_AMPLIFIER', 'RARE')}</td>
+          {inventory.countType('LINK_AMPLIFIER', 'VERY_RARE') ? <td>{inventory.countType('LINK_AMPLIFIER', 'VERY_RARE')}</td> : null}
+          <td>{inventory.countType('ULTRA_LINK_AMP')}</td>
+          <td>{inventory.countType('TRANSMUTER_DEFENSE')}</td>
+          <td>{inventory.countType('TRANSMUTER_ATTACK')}</td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <th>HeatSink</th>
+          <th className="rarity_C">C</th>
+          <th className="rarity_R">R</th>
+          <th className="rarity_VR">VR</th>
+          <th>MultiHack</th>
+          <th className="rarity_C">C</th>
+          <th className="rarity_R">R</th>
+          <th className="rarity_VR">VR</th>
+        </tr>
+        <tr>
+          <th>{inventory.countType('HEATSINK')}</th>
+          {['COMMON', 'RARE', 'VERY_RARE'].map((k) => (
+            <td>{inventory.countType('HEATSINK', k)}</td>
+          ))}
+          <th>{inventory.countType('MULTIHACK')}</th>
+          {['COMMON', 'RARE', 'VERY_RARE'].map((k) => (
+            <td>{inventory.countType('MULTIHACK', k)}</td>
+          ))}
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <th>Capsule</th>
+          <th>Quantum</th>
+          <th>KeyLocker</th>
+          <th>Kinetic</th>
+          <th>Media</th>
+        </tr>
+        <tr>
+          <td>{inventory.countType('CAPSULE')}</td>
+          <td>{inventory.countType('INTEREST_CAPSULE')}</td>
+          <td>{inventory.countType('KEY_CAPSULE')}</td>
+          <td>{inventory.countType('KINETIC_CAPSULE')}</td>
+          <td>{inventory.countType('MEDIA')}</td>
+        </tr>
+      </table>
+      <table>
+        <tr>
+          <th>Apex</th>
+          <th>Fracker</th>
+          <th className="rarity_R">BB R</th>
+          <th className="rarity_VR">BB VR</th>
+          <th>Beacon</th>
+          <th>FW ENL</th>
+          <th>FW RES</th>
+        </tr>
+        <tr>
+          <td>{inventory.countType('PLAYER_POWERUP:APEX')}</td>
+          <td>{inventory.countType('PORTAL_POWERUP:FRACK')}</td>
+          <td>{inventory.countType('PORTAL_POWERUP:BB_BATTLE_RARE')}</td>
+          <td>{inventory.countType('PORTAL_POWERUP:BB_BATTLE')}</td>
+          <td>{beacon}</td>
+          <td>{inventory.countType('PORTAL_POWERUP:FW_ENL')}</td>
+          <td>{inventory.countType('PORTAL_POWERUP:FW_RES')}</td>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
 // eslint-disable-next-line no-unused-vars
