7370d659f4c82a9e1ca46a4ce877d821adeff724
LeJeu <64744459+le-jeu@users.noreply.github.com>
2022-11-13 19:36:02 +0100

    fix: error on portalSelected

--- plugins/ingress-icons.js
+++ plugins/ingress-icons.js
@@ -1,14 +1,11 @@
 // @author         jaiperdu
 // @name           Ingress Icons
 // @category       Appearance
-// @version        0.1.1
+// @version        0.1.2
 // @description    Bring ameba64/ingress-items icons into IITC
-
 
 function getModList(d) {
   var mods = [];
-  var modsTitle = [];
-  var modsColor = [];
 
   for (var i = 0; i < 4; i++) {
     var mod = d.mods[i];
@@ -17,7 +14,7 @@ function getModList(d) {
       name: '',
       class: 'mod_free_slot',
       tooltip: '',
-    }
+    };
 
     if (mod) {
       // all mods seem to follow the same pattern for the data structure
@@ -25,22 +22,26 @@ function getModList(d) {
 
       item.name = mod.name || '(unknown mod)';
 
-      item.class = mod.name.toLowerCase().replace('(-)', 'minus').replace('(+)', 'plus').replace(/[^a-z]/g, '_');
+      item.class = mod.name
+        .toLowerCase()
+        .replace('(-)', 'minus')
+        .replace('(+)', 'plus')
+        .replace(/[^a-z]/g, '_');
 
       if (mod.rarity) {
-        item.name = mod.rarity.capitalize().replace(/_/g,' ') + ' ' + item.name;
+        item.name = mod.rarity.capitalize().replace(/_/g, ' ') + ' ' + item.name;
         item.class = item.class + ' ' + mod.rarity.toLowerCase();
       }
 
       item.tooltip = item.name + '\n';
       if (mod.owner) {
-        item.tooltip += 'Installed by: '+ mod.owner + '\n';
+        item.tooltip += 'Installed by: ' + mod.owner + '\n';
       }
 
       if (mod.stats) {
         item.tooltip += 'Stats:';
         for (var key in mod.stats) {
-          if (!mod.stats.hasOwnProperty(key)) continue;
+          if (!(key in mod.stats)) continue;
           var val = mod.stats[key];
 
           // if (key === 'REMOVAL_STICKINESS' && val == 0) continue;  // stat on all mods recently - unknown meaning, not displayed in stock client
@@ -55,7 +56,7 @@ function getModList(d) {
           else if (key === 'REMOVAL_STICKINESS' && val > 100) val = (val/10000)+'%'; // an educated guess
           // else display unmodified. correct for shield mitigation and multihack - unknown for future/other mods
 
-          item.tooltip += '\n+' +  val + ' ' + key.capitalize().replace(/_/g,' ');
+          item.tooltip += '\n+' + val + ' ' + key.capitalize().replace(/_/g, ' ');
         }
       }
     }
@@ -69,7 +70,7 @@ function getModList(d) {
 function getModDetails(d) {
   var t = '';
   getModList(d).forEach(function (item) {
-    t += '<span'+(item.tooltip.length ? ' title="'+item.tooltip+'"' : '')+' class="'+item.class+'"></span>'
+    t += '<span' + (item.tooltip.length ? ' title="' + item.tooltip + '"' : '') + ' class="' + item.class + '"></span>';
   });
 
   return t;
@@ -80,20 +81,22 @@ function updateMobile(data) {
   if (el) el.remove();
 
   var guid = data.selectedPortalGuid;
-  if(!window.portals[guid]) return;
+  if (!window.portals[guid]) return;
 
   var details = window.portalDetail.get(guid);
   var t = '';
-  getModList(details).forEach(function (item) {
-    t += '<div'+(item.tooltip.length ? ' title="'+item.tooltip+'"' : '')+' class="'+item.class+'"></div>'
-  });
+  if (details) {
+    getModList(details).forEach(function (item) {
+      t += '<div' + (item.tooltip.length ? ' title="' + item.tooltip + '"' : '') + ' class="' + item.class + '"></div>';
+    });
+  }
 
-  $('#updatestatus').prepend('<div class="mods">'+t+'</div>');
+  $('#updatestatus').prepend('<div class="mods">' + t + '</div>');
 }
 
-var setup = function () {
+function setup() {
   $('<style>').prop('type', 'text/css').html('@include_string:ingress-icons.css@').appendTo('head');
 
   window.getModDetails = getModDetails;
-  if (isSmartphone()) window.addHook('portalSelected', updateMobile);
+  if (window.isSmartphone()) window.addHook('portalSelected', updateMobile);
 }
