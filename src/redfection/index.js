7c97a487e887c58e221c80625c30fe653ee9ffc5
LeJeu <64744459+le-jeu@users.noreply.github.com>
2022-11-13 23:28:01 +0100

    redfection: add obfuscated name

--- src/redfection/index.js
+++ src/redfection/index.js
@@ -34,14 +34,24 @@ function setup() {
   window.TEAM_MAC = 3;
   window.COLORS[window.TEAM_MAC] = '#ff0028';
   window.TEAM_TO_CSS[window.TEAM_MAC] = 'mac';
-  window.TEAM_NAMES[window.TEAM_MAC] = 'U̶͚̓̍N̴̖̈K̠͔̍͑̂͜N̞̥͋̀̉Ȯ̶̹͕̀W̶̢͚͑̚͝Ṉ̨̟̒̅ ';
+  window.TEAM_NAMES[window.TEAM_MAC] =
+    'U\u0336\u035aN\u0334\u0316K\u0320\u0354N\u031e\u0325\u022e\u0336\u0339W\u0336\u0322\u1e48\u0328\u031f';
 
   window.teamStringToId = teamStringToId;
 
   const decodePortal = window.decodeArray.portal;
   window.decodeArray.portal = function (a, details) {
-    if (a.length > 6 && a[1] === 'N' && a[6] > 0) a[1] = 'M';
-    return decodePortal(a, details);
+    const res = decodePortal(a, details);
+    if (res.team === 'N' && res.resCount) {
+      res.team = 'M';
+      if (res.owner) res.owner = window.TEAM_NAMES[window.TEAM_MAC];
+      if (res.resonators) {
+        for (const r of res.resonators) {
+          r.owner = window.TEAM_NAMES[window.TEAM_MAC];
+        }
+      }
+    }
+    return res;
   };
 
   const css = document.createElement('style');
