cbbba952bc1cecd14a1b7b5c0e3ec68ec047f4b8
LeJeu <64744459+le-jeu@users.noreply.github.com>
2022-11-14 11:22:58 +0100

    redfection: simple style for portals list

--- src/redfection/index.js
+++ src/redfection/index.js
@@ -60,7 +60,9 @@ function setup() {
   };
 
   const css = document.createElement('style');
-  css.textContent = '.mac { color: #f74a4a }';
+  css.textContent = 
+    '.mac { color: #f74a4a }\n'
+    + '#portalslist table tr.mac td { background-color: #a22121 }';
   document.head.append(css);
 }
 
